297245008e997bc5b700f70fb3f18164514b73b8
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-27 22:33:41 +0600

    scope output based question solved

--- 1 - JavaScript Interview/3 - Functions/script.js
+++ 1 - JavaScript Interview/3 - Functions/script.js
@@ -60,3 +60,19 @@ function getScore() {
 }
 
 console.log(getScore());
+
+// Q-2
+for (let i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, i * 1000);
+}
+// 0, 1, 2, 3, 4
+
+for (var i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, i * 1000);
+}
+
+// 5, 5, 5, 5, 5
