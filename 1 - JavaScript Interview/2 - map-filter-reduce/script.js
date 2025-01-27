d0107cc256badc47977b83964737f009df460e30
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-27 21:17:20 +0600

    what is map()

---
+++ 1 - JavaScript Interview/2 - map-filter-reduce/script.js
@@ -0,0 +1,3 @@
+//  map, filter and reduce
+// what is map() ?
+// map() creates a new array from calling a function for every array element. map() does not execute the function for empty elements. map() does not change the original array.
