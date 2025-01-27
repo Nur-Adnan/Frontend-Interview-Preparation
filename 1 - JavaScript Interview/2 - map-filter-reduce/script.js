963c2603f4f2060d75a1f78b974269120b10f838
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-27 21:36:31 +0600

    reduce method completed

--- 1 - JavaScript Interview/2 - map-filter-reduce/script.js
+++ 1 - JavaScript Interview/2 - map-filter-reduce/script.js
@@ -24,3 +24,10 @@ console.log(moreThanTwo);
 // The array reduce in JavaScript is a predefined method used to reduce an array to a single value by passing a callback function on each element of the array. It accepts a function executed on all the items of the specified array in the left-to-right sequence. The returned single value is stored in the accumulator.
 
 const numsThree = [1, 2, 3, 4];
+const sum = numsThree.reduce((acc, curr, index, arr) => {
+  // accumulator is the result of the previous computation
+  // if there's no initial value, it takes first element of array as initial value for accumulator
+  return acc + curr;
+}, 0);
+
+console.log(sum);
