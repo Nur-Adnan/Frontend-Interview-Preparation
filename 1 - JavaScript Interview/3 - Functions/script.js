a7b0340dff3c24e0f8f3378e36be52f3493d05a5
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-27 22:13:35 +0600

    function dec and init completed

---
+++ 1 - JavaScript Interview/3 - Functions/script.js
@@ -0,0 +1,10 @@
+// function is Js
+// What is function Declaration, defination and statement
+function square(num) {
+  return num * num;
+}
+// which has no name, then we call it anonymas
+// What is function expression
+const square = function (num) {
+  return num * num;
+};
