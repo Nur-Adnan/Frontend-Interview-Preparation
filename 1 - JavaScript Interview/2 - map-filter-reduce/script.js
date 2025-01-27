e44bca9650fb9342efc7d29398def7c5e1a42cdd
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-27 21:32:46 +0600

    reduce init

--- 1 - JavaScript Interview/2 - map-filter-reduce/script.js
+++ 1 - JavaScript Interview/2 - map-filter-reduce/script.js
@@ -2,9 +2,9 @@
 // what is map() ?
 // map() creates a new array from calling a function for every array element. map() does not execute the function for empty elements. map() does not change the original array.
 
-const nums = [1, 2, 3, 4];
+const numsOne = [1, 2, 3, 4];
 
-const multiplyThree = nums.map((currentElement, index, arr) => {
+const multiplyThree = numsOne.map((currentElement, index, arr) => {
   return currentElement * 3 + index;
 });
 console.log(multiplyThree);
@@ -12,3 +12,15 @@ console.log(multiplyThree);
 // what is filter() ?
 // The filter() method creates a new array filled with elements that pass a test provided by a function.
 
+const numsTwo = [1, 2, 3, 4];
+
+const moreThanTwo = numsTwo.filter((num) => {
+  return num > 2;
+});
+
+console.log(moreThanTwo);
+
+// what is reduce() ?
+// The array reduce in JavaScript is a predefined method used to reduce an array to a single value by passing a callback function on each element of the array. It accepts a function executed on all the items of the specified array in the left-to-right sequence. The returned single value is stored in the accumulator.
+
+const numsThree = [1, 2, 3, 4];
