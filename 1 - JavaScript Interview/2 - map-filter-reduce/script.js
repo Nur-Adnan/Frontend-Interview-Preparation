f0ea2f6abe336490090d122fa9ce2ae17e04c0d3
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-27 22:07:42 +0600

    completed map, filter and Reduce

--- 1 - JavaScript Interview/2 - map-filter-reduce/script.js
+++ 1 - JavaScript Interview/2 - map-filter-reduce/script.js
@@ -92,3 +92,72 @@ const sums = numsSeven.myReduce((acc, curr, index, arr) => {
 }, 0);
 
 console.log(sums);
+
+// Question 7 : map vs foreach
+
+let students = [
+  { name: "Piyush", rollNumber: 31, marks: 80 },
+  { name: "Jenny", rollNumber: 15, marks: 69 },
+  { name: "Kaushal", rollNumber: 16, marks: 35 },
+  { name: "Dilpreet", rollNumber: 7, marks: 55 },
+];
+
+// Q1 - Return only the names of students in capital
+
+// Solution 1 : Traditional for() loop
+
+let namesOne = [];
+for (let index = 0; index < students.length; index++) {
+  namesOne.push(students[index].name.toUpperCase());
+}
+
+console.log(namesOne);
+
+// Solution 2 : forEach()
+let namesTwo = [];
+students.forEach((student) => {
+  namesTwo.push(student.name.toUpperCase());
+});
+
+console.log(namesTwo);
+
+// Solution 3 : map()
+let namesThree = students.map((stu) => stu.name.toUpperCase());
+console.log(namesThree);
+
+// Q2 - we want to get the details of students who scored more than 60 marks.
+
+let namesFour = students.filter((stu) => stu.marks > 60);
+console.log(namesFour);
+
+// Q3 - Get the details of students who scored more than 60 marks and have rollNumber greater than 15.
+
+let namesFive = students.filter((stu) => stu.marks > 60 && stu.rollNumber > 20);
+console.log(namesFive);
+
+// Q4 - Sum total of the marks of the students
+
+let totalMarks = students.reduce((acc, emp) => acc + emp.marks, 0);
+console.log(totalMarks);
+
+// Q5 - Get only the names of the students who scored more than 60 marks
+
+//  all the functions allow chaining
+let namesEight = students
+  .filter((stu) => stu.marks > 60)
+  .map((stu) => stu.name);
+console.log(namesEight);
+
+// Q6 - print the total marks of the students with marks greater than 60 after 20 marks has been added to those students who scored less than 60.
+
+let totalMark = students
+  .map(function (stu) {
+    if (stu.marks < 60) {
+      stu.marks += 20;
+    }
+    return stu;
+  })
+  .filter((stu) => stu.marks > 60)
+  .reduce((acc, curr) => acc + curr.marks, 0);
+
+console.log(totalMark);
