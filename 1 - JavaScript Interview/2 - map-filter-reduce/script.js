6b129876bc81f3b1026fbb4f7283e566aeb5bb87
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-27 21:26:12 +0600

    map completed

--- 1 - JavaScript Interview/2 - map-filter-reduce/script.js
+++ 1 - JavaScript Interview/2 - map-filter-reduce/script.js
@@ -3,3 +3,8 @@
 // map() creates a new array from calling a function for every array element. map() does not execute the function for empty elements. map() does not change the original array.
 
 const nums = [1, 2, 3, 4];
+
+const multiplyThree = nums.map((currentElement, index, arr) => {
+  return currentElement * 3 + index;
+});
+console.log(multiplyThree);
