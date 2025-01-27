911cc1cdd708eeee7a5ae8df7e8fd1b398fcfa6f
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-27 22:17:41 +0600

    first class functions completed

--- 1 - JavaScript Interview/3 - Functions/script.js
+++ 1 - JavaScript Interview/3 - Functions/script.js
@@ -1,10 +1,22 @@
 // function is Js
 // What is function Declaration, defination and statement
-function square(num) {
+function squareOne(num) {
   return num * num;
 }
 // which has no name, then we call it anonymas
 // What is function expression
-const square = function (num) {
+const squareTwo = function (num) {
   return num * num;
 };
+
+// What are first class functions
+// first class functions are those function which are act like a variable . The variable is onverted into a particular function.
+
+function squareThree(num) {
+  return num * num;
+}
+
+function displaySquare(fn) {
+  console.log("Square is " + fn(5));
+}
+displaySquare(squareThree);
